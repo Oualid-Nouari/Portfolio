5733741ccf6741b30b3342a1aaf5eba275116806
WALiiD enouari <nouarioualid1@gmail.com>
2023-04-26 11:36:05 +0000

    a

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ import PlateLogo from '../imgs/logoMe.png'
 const Header = () => {
   return (
     <header>
-        <a href='/'><img src={PlateLogo} alt="" width="150px" /></a>
+        <a href='/Portfolio'><img src={PlateLogo} alt="" width="150px" /></a>
         <ul>
             <li><a href='#about-me'><i class="fa-solid fa-info"></i><span> About Me</span></a></li>
             <li><a href='#project'><i class="fa-solid fa-list-check"></i><span> Project</span></a></li>
