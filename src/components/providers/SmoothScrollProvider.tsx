be4d92c3d5dc5e045413a787f74e75f5398d70ce
Oualid Nouari <oualidnouari0@gmail.com>
2026-05-23 12:49:23 +0100

    Fix a deployment error in vercel

--- src/components/providers/SmoothScrollProvider.tsx
+++ src/components/providers/SmoothScrollProvider.tsx
@@ -21,7 +21,6 @@ export default function SmoothScrollProvider({
       smoothWheel: true,
       wheelMultiplier: 1,
       touchMultiplier: 2,
-      autoStart: false,
     });
 
     lenisRef.current = lenis;
